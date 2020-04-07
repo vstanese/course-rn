a99a4c03b54bff496d71c94d7c1b305dd23e9edf
Dan Ichim <ichimdanconstantin@gmail.com>
2020-04-07 14:12:39 +0300

    add playground

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import BoxesScreen from "./src/screens/BoxesScreen";
 import FlexDemo from "./src/screens/FlexDemo";
+import PlaygroundScreen from "./src/screens/PlaygroundScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Boxes: BoxesScreen,
     Flex: FlexDemo,
+    Play: PlaygroundScreen,
   },
   {
     initialRouteName: "Home",
