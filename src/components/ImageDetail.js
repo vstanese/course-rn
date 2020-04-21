39b9718e0288162a364ed6ee37db9a09fee03fdf
Dan Ichim <ichimdanconstantin@gmail.com>
2020-04-21 15:51:21 +0300

    ImageDetail component

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const ImageDetail = () => {
+  return (
+    <View>
+      <Text>Image Detail</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create();
+
+export default ListsScreen;
