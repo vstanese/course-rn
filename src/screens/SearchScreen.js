ab56cce68d5e9d40f7ec212934ca12fd34f82bb7
Dan Ichim <ichimdanconstantin@gmail.com>
2020-05-05 14:00:01 +0300

    error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,22 +6,29 @@ import v3 from "../api/v3";
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [errorMsg, setErrorMsg] = useState("");
 
   const searchApi = async () => {
-    const response = await v3.get("/search", {
-      params: {
-        limit: 50,
-        term,
-        location: "NYC",
-      },
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await v3.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "NYC",
+        },
+      });
+      setResults(response.data.businesses);
+      setErrorMsg(null);
+    } catch (e) {
+      console.log(e);
+      setErrorMsg("Smth went wrong");
+    }
   };
 
   return (
     <View style={{ backgroundColor: "#fff", flex: 1 }}>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Search Screen</Text>
+      {errorMsg ? <Text>{errorMsg}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
